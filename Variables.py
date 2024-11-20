c8d4197dc19947b13dcb0991db57683b8d1f8be3
Sai krishna sandu <saikrishnasskm@gmail.com>
2024-11-20 11:36:02 -0500

    My first commit

---
+++ Variables.py
@@ -0,0 +1,50 @@
+x=5
+y='john'
+print(x)
+print(y)
+
+
+
+# variables do not need to be declared with any particular type,and can even change after they have been set.
+
+x=4
+x='salary' #here first we gave x = 4 and changed to x = salary which is a string 
+print(x)
+
+
+   # Casting  or converting 
+#if you want to specify the data type of a variable,this can be done with casting.
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x, "-->",y,"-->", z)
+
+
+
+# Get the type
+ # By using type() function we can get the data type of a variable.
+
+x=5
+y='john'
+z=5.6
+print(type(x),type(y),type(z))
+
+
+# single or double quotes
+# String variables can be declared either by using single or double quotes
+
+x="john"
+x='john' #both are same
+
+
+
+#Case sensitive
+# variable name are case sensitive
+# this will create new varible
+a=4
+A="salary"
+print(a,A)
+
+
+
