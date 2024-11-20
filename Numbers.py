da49efe67069ab367365fcdecfeb8da0a6c04352
Sai krishna sandu <saikrishnasskm@gmail.com>
2024-11-20 11:11:56 -0500

    INITIAL COMMIT

---
+++ Numbers.py
@@ -0,0 +1,73 @@
+""" 
+  int 
+
+  float
+
+  complex
+
+
+"""
+
+
+
+x = 1
+y = 3.4
+z = 10j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# int
+
+x = 10
+y=2824897764788924
+z = -29292
+
+print(type(y))
+print(type(z))
+
+
+# float
+
+x = 1.10
+y = -34.46
+x = 35e4
+print(type(x))
+y = 12E4
+
+print(type(y))
+z = -37.1e5
+print(type(z))
+
+
+
+
+#complex
+
+x = 3+4j
+y= 5j
+z= -67j
+
+print(type(z))
+
+
+
+# type conversion
+
+
+x = 5
+y=2.6
+z=6j
+a=float(x)
+b = int(y)
+c=complex(x)
+
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+print(x,y,z)
